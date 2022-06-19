f0cbd05406ff4744581533d9146122e1261be672
Jarrett Hopkins <jarrettphopkins@Gmail.com>
2022-06-19 19:05:42 -0400

    version 2.0

---
+++ server.js
@@ -0,0 +1,68 @@
+//starts import express package and initialize app variable by setting it to value of express()
+const express = require('express');
+const app = express();
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const PORT = process.env.PORT || 3000;
+const host = '0.0.0.0';
+const fs = require('fs');
+
+//this is require db.json file stuff
+let db = require('./db/db.json');
+
+//this is the middleware
+app.use(express.json());
+app.use(express.static('public'));
+
+//add static routes here for the notes.html page and index.html page respectively
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '/public/notes.html')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+// GET API route
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+})
+
+//Post API route
+app.post('/api/notes', (req, res) => {
+    req.body.id = uuidv4();
+    db.push(req.body);
+    fs.writeFile('./db/db.json', JSON.stringify(db), function (err) {
+        if (err) throw err
+        console.log('Finished!')
+    })
+    res.json(req.body);
+})
+
+//Delete API route
+app.delete('/api/notes/:id', (req, res) => {
+    const { id } = req.params;
+
+    //iterate over db array to compare ids. if ids are equal, delete from array
+    for (let i = 0; i < db.length; i++) {
+        if (id === db[i].id) {
+            db.splice(i, 1)
+        } else {
+            res.status(404).send();
+        }
+    }
+
+    //here I write new db array to db.json file
+    fs.writeFile('./db/db.json', JSON.stringify(db), function (err) {
+        if (err) throw err
+    })
+
+    res.send('DONE!');
+})
+
+app.listen(PORT, host, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
+
+
